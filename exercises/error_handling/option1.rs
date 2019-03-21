e96d613e4620dac9442fca45be39ff112a2c826b
vijark <th0m4s97@gmx.de>
2019-03-21 23:14:56 +0100

    Add files via upload

---
+++ exercises/error_handling/option1.rs
@@ -0,0 +1,47 @@
+// option1.rs
+// This example panics because the second time it calls `pop`, the `vec`
+// is empty, so `pop` returns `None`, and `unwrap` panics if it's called
+// on `None`. Handle this in a more graceful way than calling `unwrap`!
+// Scroll down for hints :)
+
+fn main() {
+    let mut list = vec![3];
+
+    if let Some(last) = list.pop() {
+        println!("The last item in the list is {:?}", last);
+    }
+
+    if let Some(second_to_last) = list.pop() {
+        println!("The second-to-last item in the list is {:?}", second_to_last);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Try using a `match` statement where the arms are `Some(thing)` and `None`.
+// Or set a default value to print out if you get `None` by using the
+// function `unwrap_or`.
+// Or use an `if let` statement on the result of `pop()` to both destructure
+// a `Some` value and only print out something if we have a value!
